c2341cdd10ddb009938477423f3de186c3fef81e
burakgokcinarr <burakgokcinar@gmail.com>
2024-08-03 20:09:01 +0300

    contact page edited.

--- src/app/(tabs)/contacts.jsx
+++ src/app/(tabs)/contacts.jsx
@@ -1,5 +1,5 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigation } from 'expo-router'
 import { readData, fetchFilteredUsers } from '../../api/Api'
@@ -9,6 +9,7 @@ export default function contacts() {
   const navigation = useNavigation();
   const dispatch   = useDispatch();
   const userInfo   = useSelector((state) => state.auth.user)
+  const [userList, setUserList] = useState([]);
 
   useEffect(() => {
     allUserData(); 
@@ -16,14 +17,21 @@ export default function contacts() {
 
   const allUserData = async() => {
     const filter = [{ type: 'neq', column: 'useruuid', value: userInfo.id }]
-    const data = await readData("tbl_User", "*", filter)
-
+    const {data} = await readData("tbl_User", "*", filter)
+    setUserList(data);
     console.log(data)
   }
 
   return (
     <View style={styles.container}>
       <Text>contacts</Text>
+      {
+        userList.map((user, index) => {
+          return (
+            <Text key={index}>{user.username}</Text>
+          )
+        })
+      }
     </View>
   )
 }
